c5d9f163d52e57fbceecd731e4bcb2c252e82056
ozzy <ozzy@ozzy.(none)>
2012-12-11 12:33:02 +0530

    Dominance Tree Calculator untested

--- src/DominanceTreeConstructor.cpp
+++ src/DominanceTreeConstructor.cpp
@@ -3,19 +3,20 @@
 #include "debug.h"
 #include <map>
 #include <set>
-//#include <densemap.h>
-//
-using llvm::BasicBlock;
+#include <llvm/ADT/DepthFirstIterator.h> //For iterating through the predecessors of the basic block
+#include <llvm/Support/CFG.h> //We need a specialization of the GraphTrait class 
+
 using namespace std;
+using namespace llvm;
 char DominanceTreeConstructor::ID = 0;
 
 static Trace& gTrace = Trace::getInstance();
 
-bool DominanceTreeConstructor::runOnFunction(llvm::Function &F){
+bool DominanceTreeConstructor::runOnFunction(Function &F){
 	gTrace<<" Constructing Dominator Tree for "<<F.getName();
 
-    llvm::Function::BasicBlockListType& bbList = F.getBasicBlockList();
-    llvm::Function::BasicBlockListType::iterator iter = bbList.begin();
+    Function::BasicBlockListType& bbList = F.getBasicBlockList();
+    Function::BasicBlockListType::iterator iter = bbList.begin();
     /*
      * Create a multimap of nodes mapped to their dominators.
      * Set the dominator of each node to be itself
@@ -29,22 +30,25 @@ bool DominanceTreeConstructor::runOnFunction(llvm::Function &F){
         domSet->insert(&*iter);
         dom.insert(pair<BasicBlock*, std::set<BasicBlock*>*>(&*iter, domSet));
     }
-    bool changed = true;
 
+    bool changed = true;
     while(changed){
         changed = false;
-        for(iter = bbList.begin() ; iter != F.getBasicBlockList().end(); ++iter){
-            std::set<BasicBlock*> *domSet = dom[&*iter];
-            int numElts = domSet->size();
-            for(int i = 0 ; i < numElts ; ++i){
-                
-            }
-            
-            if(domSet->size() != numElts)
-                changed = true;
-                        
+        for(Function::iterator bbIter = F.begin() ; bbIter != F.end(); ++bbIter){ //for each basic block 
+                std::set<BasicBlock*> newDom;
+                BasicBlock* bb = dynamic_cast<BasicBlock*>(&*bbIter);
+                for(idf_iterator<BasicBlock*> predIter = idf_begin(bb); //for each of its predecessor
+                                              predIter != idf_end(bb); 
+                                              ++predIter){
+                    std::set<BasicBlock*> *predDom = dom[*predIter];
+                    for(std::set<BasicBlock*>::iterator predDomIter = predDom->begin() ; predDomIter != predDom->end(); ++predDomIter){
+                        newDom.insert(*predDomIter);
+                    }
+                }
+                newDom.insert(bb); //Union n
+                if(newDom != *dom[bb])
+                    changed = true;
         }
     }
-
 	return false;
 }
